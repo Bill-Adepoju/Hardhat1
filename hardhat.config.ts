b2d270b5f2fb2cb275aef2c3e82dbee76e176172
Bill <wyze97@gmail.com>
2023-02-27 12:05:26 +0100

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+
+  networks:{
+    hardhat:{
+
+    },
+    
+    goerli :{
+      url:process.env.HTTP_URL,
+      //@ts-ignore
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  },
+};
+
+export default config;
